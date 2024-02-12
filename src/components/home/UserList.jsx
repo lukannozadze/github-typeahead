64a7493544f36a93e3eaed0ea94849e8eae159aa
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-02-12 13:00:41 +0400

    small changes in home page design

--- src/components/home/UserList.jsx
+++ src/components/home/UserList.jsx
@@ -1,23 +1,24 @@
-const UserList = ({value})=> {
+const UserList = ({ value }) => {
+  const filteredUsers = value?.items.slice(0, 5); //should be changed
 
-   const filteredUsers = value?.items.slice(0,5); //should be changed
-   
+  console.log(value);
 
   return (
-    <ul className='flex flex-col gap-8'>
-     {filteredUsers?.map((user)=>{
-     return <li key={user.id}>
-      <div className='bg-[#1E2A47] rounded-md px-4 py-4 max-w-[700px] min-w-[500px]'>
-        <div className='flex items-center justify-between'>
-          <h2 className='text-white font-bold'>{user.login}</h2>
-          <img className='w-20 h-20 rounded-full' src={user.avatar_url}/>
-        </div>
-        <a className='text-gray-500  hover:text-blue-400 text-md' href={user.url}>{user.url}</a>
-      </div>
-      </li>     
-     })}
+    <ul className="flex flex-col gap-8">
+      {filteredUsers?.map((user) => {
+        return (
+          <li key={user.id}>
+            <div className="px-4 py-4 max-w-[700px] min-w-[500px] flex items-center justify-between bg-[#1E2A47] rounded-md shadow-xl">
+              <h2 className="text-white font-bold text-xl font-mono">
+                {user.login}
+              </h2>
+              <img className="w-20 h-20 rounded-full" src={user.avatar_url} />
+            </div>
+          </li>
+        );
+      })}
     </ul>
-  )
-}
+  );
+};
 
 export default UserList;
