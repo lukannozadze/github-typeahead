d22c4875c128b333543b9dd61a55f5cc06f770ca
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-02-15 17:48:02 +0400

    finish detail page design

--- src/components/user/Info.jsx
+++ src/components/user/Info.jsx
@@ -16,14 +16,17 @@ export default function Info() {
          <img className='w-40 h-40 rounded-full' src={activeUser.avatar_url}/>
          <div className='flex flex-col gap-4' >
          <h1 className='text-white font-bold text-[32px] leading-[24px]'>{activeUser.login}</h1>
-         <p className='text-[#1079F9] '>{activeUser.id}</p>
-         <p className='text-[#1079F9]'>Score:{activeUser.score}</p>
+         <p className='text-[#1079F9] '>ID: {activeUser.id}</p>
+         <p className='text-[#1079F9]'>Score: {activeUser.score}</p>
+        
          </div>
          
     
         </div>
         <a className='w-40'  href={activeUser.html_url}><Button>Visit Github Profile</Button></a>
         <a className='w-40' href={activeUser.url}><Button variant='ghost' className='bg-[#60ABFF] text-bold text-white text-base'>Personal Info</Button></a>
+        <p className='text-white font-bold'>Type: {activeUser.type}</p>
+        <p className='text-white font-bold'>Node ID: {activeUser.node_id}</p>
     </div>
   )
 }
--- src/components/user/Layout.jsx
+++ src/components/user/Layout.jsx
@@ -1,7 +1,7 @@
 export default function Layout({children}) {
   return (
     <div className="py-16 flex justify-center items-center">
-    <section className="w-[740px] h-[500px] bg-[#1E2A47]">
+    <section className="w-[740px] h-[500px] bg-[#1E2A47]  rounded-lg">
         {children}
     </section>
     </div>
